ef69edc93254869ca5170d94b3ac1a39df057569
Youssef Elhaw <jooelhaw@gmail.com>
2025-02-22 10:02:48 +0200

    ro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from '@angular/router';
-import { AuthComponent } from './core/layouts/auth/auth.component';
-import { UserComponent } from './core/layouts/user/user.component';
-import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { LoginComponent } from './core/auth/components/login/login.component';
 import { RegisterComponent } from './core/auth/components/register/register.component';
-import { ProductsListComponent } from './features/product/components/products-list/products-list.component';
+import { AuthComponent } from './core/layouts/auth/auth.component';
+import { UserComponent } from './core/layouts/user/user.component';
 import { HomeComponent } from './features/home/components/home/home.component';
 import { ProductDetailsComponent } from './features/product/components/product-details/product-details.component';
-import { RenderMode } from '@angular/ssr';
+import { ProductsListComponent } from './features/product/components/products-list/products-list.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -25,7 +24,7 @@ export const routes: Routes = [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
       { path: 'products', component: ProductsListComponent },
-      { path: 'product-details/:id', component: ProductDetailsComponent, data: {RenderMode: 'ssr'} },
+      { path: 'product-details/:id', component: ProductDetailsComponent },
       { path: '**', component: NotFoundComponent },
     ],
   },
